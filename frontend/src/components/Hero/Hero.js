14334329471a862232e5762fd039398fd6a84d54
Dravani <42421220+Dravani@users.noreply.github.com>
2025-04-18 00:28:25 -0400

    Update Hero.js

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
       </div>
       <h2 className='heading2'>Think Green. Chat Clean.</h2>
       <div className='button-container'>
-          <a className='landing-button-download' href='https://cise.ufl.edu/~t.lu/cis4930/extension/greengpt.zip' target='_blank' rel='noreferrer'>Download</a>
+          <a className='landing-button-download' href='https://cise.ufl.edu/~t.lu/cis4930/extension/extension.zip' target='_blank' rel='noreferrer'>Download</a>
         <Link to="/signup">
           <button className='landing-button-signup'>Sign Up</button>
         </Link>
